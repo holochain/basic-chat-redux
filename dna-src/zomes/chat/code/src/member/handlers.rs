8a7f5e269acdf201919464bb6f591ead3d4fdbcd
Willem Olding <willemolding@gmail.com>
2019-11-22 12:15:42 +1100

    use local chain gets for own agent it

--- dna-src/zomes/chat/code/src/member/handlers.rs
+++ dna-src/zomes/chat/code/src/member/handlers.rs
@@ -5,8 +5,10 @@ use hdk::{
     holochain_json_api::json::RawString,
     holochain_persistence_api::cas::content::Address,
     AGENT_ADDRESS,
+    prelude::{QueryResult, QueryArgsOptions},
 };
 use crate::utils::{get_links_and_load_type, GetLinksLoadResult};
+use std::convert::TryFrom;
 
 pub fn handle_register(name: String, avatar_url: String) -> ZomeApiResult<Address> {
     let anchor_entry = Entry::App("anchor".into(), RawString::from("member_directory").into());
@@ -43,6 +45,20 @@ pub fn handle_get_member_profile(agent_address: Address) -> ZomeApiResult<Profil
     .map(|elem: &GetLinksLoadResult<Profile>| elem.entry.clone())
 }
 
+/// Get your own member profile from your local chain always to save network requests
 pub fn handle_get_my_member_profile() -> ZomeApiResult<Profile> {
-    handle_get_member_profile(AGENT_ADDRESS.to_string().into())
+    if let QueryResult::Entries(results) = hdk::query_result(
+        "chat_profile".into(),
+        QueryArgsOptions{ entries: true, ..Default::default()}
+    )? {
+        if let Some((_, Entry::App(_, entry_value))) = results.last() {
+            Ok(Profile::try_from(entry_value).expect("Invalid profile data encountered"))
+        } else {
+            Err(ZomeApiError::Internal(
+                "Agent does not have a profile registered".into(),
+            ))
+        }
+    } else {
+        unreachable!()
+    }
 }
